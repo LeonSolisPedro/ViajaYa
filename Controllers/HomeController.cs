df73f371b3e637ea0a32e22b8372f2022dd07b6f
Pedro León <pedro@wintercr.com>
2024-08-16 23:56:50 -0500

    Terminando la lógica del index, falta la vista con foreachs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,8 +13,42 @@ public class HomeController : Controller
         _factory = factory;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        var client = _factory.CreateClient("client");
+
+        var responses = await Task.WhenAll([
+          client.GetAsync("v1/Home/GetCarousel"),
+          client.GetAsync("v1/Home/GetPopularTours"),
+          client.GetAsync("v1/Home/GetOtherTours"),
+          client.GetAsync("v1/Home/GetGalleries"),
+          client.GetAsync("v1/Home/GetOffers")
+        ]);
+
+        var json1 = await (responses?.ElementAtOrDefault(0)?.Content?.ReadAsStringAsync() ?? Task.FromResult(""));
+        var json2 = await (responses?.ElementAtOrDefault(1)?.Content?.ReadAsStringAsync() ?? Task.FromResult(""));
+        var json3 = await (responses?.ElementAtOrDefault(2)?.Content?.ReadAsStringAsync() ?? Task.FromResult(""));
+        var json4 = await (responses?.ElementAtOrDefault(3)?.Content?.ReadAsStringAsync() ?? Task.FromResult(""));
+        var json5 = await (responses?.ElementAtOrDefault(4)?.Content?.ReadAsStringAsync() ?? Task.FromResult(""));
+
+
+        dynamic? data1 = JsonConvert.DeserializeObject(json1);
+        dynamic? data2 = JsonConvert.DeserializeObject(json2);
+        dynamic? data3 = JsonConvert.DeserializeObject(json3);
+        dynamic? data4 = JsonConvert.DeserializeObject(json4);
+        dynamic? data5 = JsonConvert.DeserializeObject(json5);
+
+
+        ViewBag.slider = data1;
+        ViewBag.popularTours = data2;
+        ViewBag.countPopular = data2?.Count ?? 0;
+        ViewBag.otherTours = data3;
+        ViewBag.countOther = data3?.Count ?? 0;
+        ViewBag.topGallery = data4;
+        ViewBag.countGallery = data4?.Count ?? 0;
+        ViewBag.offers = data5;
+        ViewBag.countOffers = data5?.Count ?? 0;
+
         return View();
     }
 
